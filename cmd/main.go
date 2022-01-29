83aa8e4b0116003d86579e1480d809a66cddd7d5
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-29 10:45:13 +0330

    sfu struct packs init

--- cmd/main.go
+++ cmd/main.go
@@ -33,6 +33,28 @@ type Candidate struct {
 	Candidate *webrtc.ICECandidate `json:"candidate"`
 }
 
+type SendAnswer struct {
+	SID    string                     `json:"sid"`
+	Answer *webrtc.SessionDescription `json:"answer"`
+}
+
+type ResponseCandidate struct {
+	Target    int                      `json:"target"`
+	Candidate *webrtc.ICECandidateInit `json:"candidate"`
+}
+
+type TrickleResponse struct {
+	Params ResponseCandidate `json:"params"`
+	Method string            `json:"method"`
+}
+
+type Response struct {
+	Params *webrtc.SessionDescription `json:"params"`
+	Result *webrtc.SessionDescription `json:"result"`
+	Method string                     `json:"method"`
+	Id     uint64                     `json:"id"`
+}
+
 func main() {
 	flag.StringVar(&addr, "a", "localhost:7000", "address to use")
 	flag.Parse()
