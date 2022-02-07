a8e1cb8046ca300f33d286981503c14dc466f79b
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-07 09:17:20 +0330

    dialer init

--- pkg/dialer/main.go
+++ pkg/dialer/main.go
@@ -1,7 +1,28 @@
 package dialer
 
-import "github.com/gorilla/websocket"
+import (
+	"flag"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/url"
+)
+
+var (
+	addr string
+)
 
 func MakeConnection() (*websocket.Conn, error) {
-	return nil, nil
+	flag.StringVar(&addr, "a", "localhost:7000", "address to use")
+	flag.Parse()
+
+	u := url.URL{
+		Scheme: "ws",
+		Host:   addr,
+		Path:   "/ws",
+	}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+
+	return c, err
 }
