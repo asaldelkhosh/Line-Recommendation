75afcab5d6366fdb75d7d0a1962307de81e83c3e
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-07 09:22:44 +0330

    documentation added for dialer package

--- pkg/dialer/main.go
+++ pkg/dialer/main.go
@@ -11,10 +11,15 @@ var (
 	addr string
 )
 
+// MakeConnection will create a websocket from the following url
+// and will return the connection.
 func MakeConnection() (*websocket.Conn, error) {
+
+	// creating the address with a flag
 	flag.StringVar(&addr, "a", "localhost:7000", "address to use")
 	flag.Parse()
 
+	// url that we use to create our websocket dialer
 	u := url.URL{
 		Scheme: "ws",
 		Host:   addr,
@@ -22,6 +27,7 @@ func MakeConnection() (*websocket.Conn, error) {
 	}
 	log.Printf("connecting to %s", u.String())
 
+	// creating the websocket dialer with dial method
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 
 	return c, err
