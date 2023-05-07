40c76c5f7c84c50561062a3c86716a729db20b36
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-07 12:54:55 +0330

    add: mysql migrate

--- http/internal/port/mysql/mysql.go
+++ http/internal/port/mysql/mysql.go
@@ -3,6 +3,8 @@ package mysql
 import (
 	"fmt"
 
+	"github.com/sweetie-pie/line-recommendation/internal/model"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -12,9 +14,19 @@ type MySQL struct {
 }
 
 func New() (*MySQL, error) {
+	// open sqlite connection
 	db, err := gorm.Open(sqlite.Open("volume/storage.db"), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to find volume: %v", err)
+	}
+
+	// migrate models
+	if err := db.AutoMigrate(
+		&model.Route{},
+		&model.Node{},
+		&model.Search{},
+	); err != nil {
+		return nil, fmt.Errorf("failed to migrate models: %v", err)
 	}
 
 	return &MySQL{
