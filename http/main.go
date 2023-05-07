7658fac39c254ec0427bd465d7c0ca795d988274
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-07 12:53:22 +0330

    update: main.go

--- http/main.go
+++ http/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"github.com/sweetie-pie/line-recommendation/internal/port/http"
@@ -10,21 +12,31 @@ import (
 )
 
 func main() {
+	var (
+		httpPort = flag.Int("port", 8080, "http port on localhost")
+	)
+
+	flag.Parse()
+
+	// creating a new fiber app
 	app := fiber.New()
 
+	// initializing database
 	db, err := mysql.New()
 	if err != nil {
 		panic(err)
 	}
 
+	// creating a handler
 	h := http.Handler{Repository: db}
 
 	app.Get("/routes", h.Routes)
 	app.Get("/searches", h.Searches)
 
-	log.Println("server start on")
+	log.Printf("server start on %d ...\n", *httpPort)
 
-	if err := app.Listen(":8080"); err != nil {
+	// starting http server
+	if err := app.Listen(fmt.Sprintf(":%d", *httpPort)); err != nil {
 		panic(err)
 	}
 }
