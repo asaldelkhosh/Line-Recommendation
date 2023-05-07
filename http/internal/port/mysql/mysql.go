f629fc30cc178d83dbc080712a6c42f10fb5a1f1
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-07 12:36:11 +0330

    init: mysql new connection

--- http/internal/port/mysql/mysql.go
+++ http/internal/port/mysql/mysql.go
@@ -1,1 +1,23 @@
 package mysql
+
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type MySQL struct {
+	conn *gorm.DB
+}
+
+func New() (*MySQL, error) {
+	db, err := gorm.Open(sqlite.Open("volume/storage.db"), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to find volume: %v", err)
+	}
+
+	return &MySQL{
+		conn: db,
+	}, nil
+}
