de09967c5ebff893329796de12d58f4717af3884
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-07 12:48:32 +0330

    init: handler methods

--- http/internal/port/http/handler.go
+++ http/internal/port/http/handler.go
@@ -1,1 +1,19 @@
 package http
+
+import (
+	"github.com/sweetie-pie/line-recommendation/internal/port/mysql"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Handler struct {
+	Repository mysql.MySQL
+}
+
+func (h *Handler) Routes(c *fiber.Ctx) error {
+	return c.Next()
+}
+
+func (h *Handler) Searches(c *fiber.Ctx) error {
+	return c.Next()
+}
