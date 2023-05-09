949f69557072914bc833d703a23346ffddcfae8e
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-09 09:52:43 +0330

    update: response

--- http/internal/port/http/response.go
+++ http/internal/port/http/response.go
@@ -1,7 +1,16 @@
 package http
 
-type RoutesResponse struct {
+import (
+	"github.com/sweetie-pie/line-recommendation/internal/model"
+)
+
+type Response struct {
+	Routes   []RouteResponse `json:"routes"`
+	Searches []RouteResponse `json:"searches"`
 }
 
-type SearchesResponse struct {
+type RouteResponse struct {
+	ID    uint       `json:"id"`
+	Start model.Node `json:"start"`
+	Stop  model.Node `json:"stop"`
 }
