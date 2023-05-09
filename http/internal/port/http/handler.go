7601e3693302471dca293ab4d2610277606cfc82
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-09 09:57:44 +0330

    add: handlers

--- http/internal/port/http/handler.go
+++ http/internal/port/http/handler.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"log"
+
 	"github.com/sweetie-pie/line-recommendation/internal/port/mysql"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,5 +13,48 @@ type Handler struct {
 }
 
 func (h *Handler) Data(c *fiber.Ctx) error {
-	return c.JSON(&Response{})
+	routes, err := h.Repository.GetRoutes()
+	if err != nil {
+		log.Panicln(err)
+
+		return fiber.ErrInternalServerError
+	}
+
+	routesResponse := make([]RouteResponse, 0)
+
+	for _, route := range routes {
+		src, _ := h.Repository.GetNode(route.StartID)
+		dest, _ := h.Repository.GetNode(route.StopID)
+
+		routesResponse = append(routesResponse, RouteResponse{
+			ID:    route.ID,
+			Start: *src,
+			Stop:  *dest,
+		})
+	}
+
+	searches, err := h.Repository.GetSearches()
+	if err != nil {
+		log.Panicln(err)
+
+		return fiber.ErrInternalServerError
+	}
+
+	searchResponse := make([]RouteResponse, 0)
+
+	for _, route := range searches {
+		src, _ := h.Repository.GetNode(route.StartID)
+		dest, _ := h.Repository.GetNode(route.StopID)
+
+		searchResponse = append(searchResponse, RouteResponse{
+			ID:    route.ID,
+			Start: *src,
+			Stop:  *dest,
+		})
+	}
+
+	return c.JSON(&Response{
+		Routes:   routesResponse,
+		Searches: searchResponse,
+	})
 }
