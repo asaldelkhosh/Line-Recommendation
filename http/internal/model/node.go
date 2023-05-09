6ebac3464bff0525b4696e613c7f2811b7803ae7
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-09 09:41:49 +0330

    add: nodes and search models

--- http/internal/model/node.go
+++ http/internal/model/node.go
@@ -2,4 +2,6 @@ package model
 
 type Node struct {
 	Base
+	X int64 `json:"x"`
+	Y int64 `json:"y"`
 }
--- http/internal/model/search.go
+++ http/internal/model/search.go
@@ -2,4 +2,6 @@ package model
 
 type Search struct {
 	Base
+	Start Node `json:"start"`
+	Stop  Node `json:"stop"`
 }
